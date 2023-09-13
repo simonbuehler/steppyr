c0aa062d32b358b8440032ec8da6a389667f5f30
simonbuehler <simon@00010111.de>
2023-09-13 16:50:54 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="steppyr",
+    version="0.2",
+    packages=find_packages(),
+    install_requires=[
+    ],
+    author="Simon Buehler",
+    author_email="simon@00010111.de",
+    description="Ein Schrittmotor-Bibliothek in Python",
+    url="https://github.com/simonbuehler/steppyr",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
